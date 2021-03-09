ae57b9ec8ec7796f0eaff88eb4637506ef252964
John Dowson <dowson.john.pony@gmail.com>
2021-03-09 23:02:07 +0300

    Bumped assembly version & synced version in the attr to it

--- CropReplant.cs
+++ CropReplant.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace CropReplant
 {
-    [BepInPlugin("com.github.johndowson.CropReplant", "CropReplant", "0.2.0")]
+    [BepInPlugin("com.github.johndowson.CropReplant", "CropReplant", "2.0.0")]
     public class CropReplant : BaseUnityPlugin
     {
         private static readonly Harmony harmony = new(typeof(CropReplant).GetCustomAttributes(typeof(BepInPlugin), false)
