308d2074e551e6aca5b10bfa986414949e62615a
Ben <51057455+Ch0z@users.noreply.github.com>
2021-09-26 19:01:18 +0200

    Adding time grow time left next to percent and colorized it

--- Patches/PlantPatches.cs
+++ Patches/PlantPatches.cs
@@ -8,6 +8,23 @@ namespace CropReplant.Patches
     [HarmonyPatch(typeof(Plant), "GetHoverText")]
     static class PlantGetHoverText_Patch
     {
+        private static string GetColour(double percentage)
+		{
+			string result = "#e74c3c";
+			if (percentage >= 25.0 && percentage <= 50.0)
+			{
+				result = "#e67e22";
+			}
+			if (percentage >= 50.0 && percentage <= 75.0)
+			{
+				result = "#f1c40f";
+			}
+			if (percentage >= 75.0 && percentage <= 100.0)
+			{
+				result = "#27ae60";
+			}
+			return result;
+		}
         static readonly FieldInfo StatusField = AccessTools.Field(typeof(Plant), "m_status");
         static string Postfix(string __result, Plant __instance)
         {
@@ -17,7 +34,10 @@ namespace CropReplant.Patches
             {
                 DateTime d = new DateTime(__instance.m_nview.GetZDO().GetLong("plantTime", ZNet.instance.GetTime().Ticks));
                 var timeSincePlanted = (ZNet.instance.GetTime() - d).TotalSeconds;
-                __result += $"\n{(int)(timeSincePlanted / __instance.m_growTimeMax * 100)}% grown";
+                var percentGrow = (int)(timeSincePlanted / __instance.m_growTimeMax * 100);
+				string colour = GetColour(percentGrow);
+				__result += $"\n<color={colour}>{percentGrow}% ~ {TimeSpan.FromSeconds(__instance.m_growTimeMax - timeSincePlanted).ToString(@"hh\:mm\:ss")}</color>";
+
             }
 
             return __result;
