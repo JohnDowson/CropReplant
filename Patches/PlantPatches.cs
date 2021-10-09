f365987bc18fde467870db582cfca51b0b623b63
JohnDowson <ichinenov@gmail.com>
2021-10-10 00:50:02 +0300

    Lerp growth status color

--- Patches/PlantPatches.cs
+++ Patches/PlantPatches.cs
@@ -1,29 +1,19 @@
 ﻿using HarmonyLib;
 using System;
 using System.Reflection;
-
+using UnityEngine;
 namespace CropReplant.Patches
 {
 #pragma warning disable IDE0051 // Remove unused private members
     [HarmonyPatch(typeof(Plant), "GetHoverText")]
     static class PlantGetHoverText_Patch
     {
-		private static string GetColour(double percentage)
+		private static string GetColour(float percentage)
 		{
-			string colour = "#e74c3c"; //Red
-			if (percentage >= 25.0 && percentage <= 50.0)
-			{
-				colour = "#e67e22"; //Orange
-			}
-			if (percentage >= 50.0 && percentage <= 75.0)
-			{
-				colour = "#f1c40f";//Yellow
-			}
-			if (percentage >= 75.0 && percentage <= 100.0)
-			{
-				colour = "#27ae60";//Green
-			}
-            return colour;
+			var red = new Color(0xE7, 0x4C, 0x3C);
+            var green = new Color(0x27, 0xAE, 0x60);
+            var color = Color.Lerp(red, green, percentage);
+            return ColorUtility.ToHtmlStringRGBA(color);
 		}
         static string Postfix(string __result, Plant __instance)
         {
@@ -33,15 +23,16 @@ namespace CropReplant.Patches
                 DateTime d = new(__instance.m_nview.GetZDO().GetLong("plantTime", ZNet.instance.GetTime().Ticks));
 				var timeSincePlanted = (ZNet.instance.GetTime() - d).TotalSeconds;
 				var growTime = __instance.GetGrowTime();
-				var percentGrow = (int)(timeSincePlanted / growTime * 100);
-				string colour = GetColour(percentGrow);
-				if (percentGrow < 100) 
+				float growProgress = (int)(timeSincePlanted / growTime);
+                string colour = GetColour(growProgress);
+                int growPercent = (int)(growProgress * 100f);
+				if (growPercent < 100) 
 				{
-					__result += $"\n<color={colour}>{percentGrow}% - {TimeSpan.FromSeconds(growTime - timeSincePlanted):hh\\:mm\\:ss}</color>";
+					__result += $"\n<color={colour}>{growPercent}% - {TimeSpan.FromSeconds(growTime - timeSincePlanted):hh\\:mm\\:ss}</color>";
 				}
-				if (percentGrow == 100)
+				if (growPercent == 100)
                 {
-                    __result += $"\n<color={colour}>{percentGrow}%</color>";
+                    __result += $"\n<color={colour}>{growPercent}%</color>";
 				}
             }
 
